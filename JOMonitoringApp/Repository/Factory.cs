498f3baf2d56925ebc1878d17c72ef5e8b6e3922
asale@pamana <px1998..ass@gmail.com>
2025-02-24 14:29:30 +0800

    Setup database connections.

---
+++ JOMonitoringApp/Repository/Factory.cs
@@ -0,0 +1,17 @@
+﻿using JOMonitoringApp.Interface;
+using JOMonitoringApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOMonitoringApp
+{
+    public class Factory
+    {
+        internal static GenericCommands mySqlGenericCommands;
+
+        public static IUsersRepository UsersRepository() => new UsersRepository(mySqlGenericCommands);
+    }
+}
---
+++ JOMonitoringApp/Repository/UsersRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using JOMonitoringApp.Interface;
+using JOMonitoringApp.Model;
+using JOMonitoringApp.Repository;
+
+namespace JOMonitoringApp
+{
+    internal class UsersRepository : IUsersRepository
+    {
+        private GenericCommands mySqlGenericCommands;
+
+        public UsersRepository(GenericCommands mySqlGenericCommands)
+        {
+            this.mySqlGenericCommands = mySqlGenericCommands;
+        }
+
+        public bool Delete(List<UsersModel> entityList)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetRecordByID(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable GetRecords()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable GetRecordsBySearch(string searchText)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IdExist(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Insert(UsersModel entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(UsersModel entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

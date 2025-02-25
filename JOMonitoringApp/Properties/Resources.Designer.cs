bfb044efec553c539bdc6b5d7952c991e6453498
asale@pamana <px1998..ass@gmail.com>
2025-02-25 16:55:32 +0800

    set icon

--- JOMonitoringApp/Properties/Resources.Designer.cs
+++ JOMonitoringApp/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace JOMonitoringApp.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon thumbnail_PAMANA_COREL1 {
+            get {
+                object obj = ResourceManager.GetObject("thumbnail_PAMANA_COREL1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap thumbnail_PAWAD_COREL {

01e555ca8790d977054742b5ea46de12d842f6c4
asale@pamana <px1998..ass@gmail.com>
2025-03-11 08:29:24 +0800

    Added model for job orders particulars

---
+++ JOMonitoringApp/Model/JobOrdersParticularsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOMonitoringApp.Model
+{
+    public class JobOrdersParticularsModel
+    {
+        public int Id { get; set; }
+
+        public int JobOrdersId { get; set; }
+
+        public int ParticularsId { get; set; }
+    }
+}
